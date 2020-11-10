d324568e5d870199d11072507a232aab8c9502e6
MAISDUSE <50581556+MAISDUSE@users.noreply.github.com>
2020-11-10 14:52:04 +0100

    tp1 fin & tp2 timer

--- JS/geometrie/point.js
+++ JS/geometrie/point.js
@@ -13,3 +13,23 @@ function point(x,y,canvas){
   this.affiche=affiche;
 
 }
+
+
+
+
+var Point = function(x,y,canvas)
+{
+	this.canvas=canvas;
+	this.canvas2dContext=this.canvas.getContext("2d");
+	this.x=x;
+	this.y=y;
+
+	this.ache = function()
+	{
+
+		this.canvas2dContext.beginPath();
+		this.canvas2dContext.arc(this.x,this.y,5,0,2*Math.PI,false);
+		this.canvas2dContext.fillStyle = "black";
+		this.canvas2dContext.stroke();
+	}
+}
