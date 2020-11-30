c798bc0faf05b6c220eebdedb9ea6a378a6a8fa2
MAISDUSE <50581556+MAISDUSE@users.noreply.github.com>
2020-11-30 10:11:45 +0100

    ????????????

--- JS/serviceworkers.js
+++ JS/serviceworkers.js
@@ -1,17 +1,27 @@
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open('v1').then(function (cache) {
-      return cache.addAll(['/JS/tpServiceWorkers.html,style.css']);
-    })
-  );
+const cacheName = 'tpJsSw';
+
+let cors_url = 'https://cors-anywhere.herokuapp.com/';
+let url = 'https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu'
+
+
+let cacheResources = [
+    '/JS/',
+    cors_url + url
+];
+
+this.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(cacheResources);
+        })
+    );
 });
 
-self.addEventListener("fetch", function (event) {
-  console.log("fetch event fired and catch : " + event.request.url);
-
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+    console.log('fetched');
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
