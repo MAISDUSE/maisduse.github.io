a964321411f5086dfcc7ac1615462bf3d6a08b55
MAISDUSE <50581556+MAISDUSE@users.noreply.github.com>
2020-11-30 09:52:14 +0100

    sw

--- JS/serviceworkers.js
+++ JS/serviceworkers.js
@@ -1,18 +1,7 @@
-var staticCacheName = "pwa";
-
-let cors_url = 'https://cors-anywhere.herokuapp.com/';
-let url = 'https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu'
-
-var filesToCache = [
-	'/',
-	'https://cors-anywhere.herokuapp.com/https://planning.univ-rennes1.fr/jsp/custom/modules/plannings/9EYlGR3a.shu'
-]
-
-
 self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
+    caches.open('v1').then(function (cache) {
+      return cache.addAll(['/JS/tpServiceWorkers.html,style.css']);
     })
   );
 });
